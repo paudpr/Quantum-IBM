73f4517469b409dff3964ecfd0667f3da71b4768
Paula <paula.p.ramirez@gmail.com>
2023-03-14 11:06:55 +0100

    module01 3

---
+++ 42AI_python/module01/ex02/test.py
@@ -0,0 +1,26 @@
+# • la suma y la resta funcionan para 2 vectores de la misma forma,
+# • la multiplicación (mul and rmul) funcionan para un vector y un escalar,
+# • la división (truediv) funciona con un vector y un escalar,
+# • la división (rtruediv) produce un error aritmético (esta prueba puede comentarse para las demás pruebas y descomentarse para mostrar esta)
+
+from vector import Vector
+
+# v1 = Vector([[3.], [0.]], (2, 1))
+# v2 = Vector([[5.], ['a.']], (2, 1))
+# v1.dot(v2)
+# v1.T()
+
+# v1 = Vector([[0.0], [1.0], [2.0], [3.0]]) 
+# v2 = v1 * 5
+# print(v2)
+
+# v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+# print(v1.shape)
+# print(v1.T())
+
+
+v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+v2 = Vector([[0.0], [1.0], [2.0], [3.0]])
+
+v3 = v1 + v2
+# print(v3.values)
--- 42AI_python/module01/ex02/vector.py
+++ 42AI_python/module01/ex02/vector.py
@@ -1,20 +1,47 @@
 class Vector:
-	def __init__(values, shape):
-		if not isinstance(values, lst):
+	def __init__(self, values):
+		if not isinstance(values, list):
 			print("Vector values must be a list")
 			exit()
 		for elem in values:
-			if not isinstance(elem, lst):
+			if not isinstance(elem, list):
 				print("Vector values must contain lists")
 				exit()
+			for thing in elem:
+				if not isinstance(thing, float):
+					if isinstance(thing, int):
+						thing = float(thing)
+					else:
+						print("Vector values contains non-float values")
+						exit()
 		self.values = values
-		if not isinstance(shape, tuple):
-			print("Shape must be a tuple")
-			exit()
-		if (shape[0] or shape[1]) != 1:
-			print("One of the dimensions must be 1 for it to be a vector")
-			exit()
-		self.shape = shape
+		x, y = len(self.values), len(self.values[0])
+		self.shape = tuple((x, y))
 
-	def dot():
-		
+	def dot(self, vec):
+		if self.shape != vec.shape:
+			print("Shapes mmust be equal for scalar product")
+			exit()
+		scal_product = 0
+		for i in range(max(self.shape)):
+			product = sum(self.values[i]) * sum(vec.values[i]) 
+			scal_product += product
+
+	# no está bien
+	# def T(self):				
+	# 	self.shape = tuple((self.shape[1], self.shape[0]))
+
+	def __add__(self, other):
+		if self.shape != other.shape:
+			print("Vectors must have same dimensions")
+			exit()
+		# i = 0
+		added = []
+		for a, b in zip(self.values, other.values):
+			added.append([a + b])
+		print(added)
+		print(range(len(added)))
+		for i in range(len(added)):
+			added[i] = sum(added[i])
+		print(added)
+		return added
